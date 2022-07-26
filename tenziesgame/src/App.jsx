95c4b89fe3a1732fc407854d0d4bb74fe2676fed
Beatriz Naranjo <beanaranjo@gmail.com>
2022-07-26 14:32:20 -0600

    Roll new dice only for unheld dice

--- tenziesgame/src/App.jsx
+++ tenziesgame/src/App.jsx
@@ -3,46 +3,69 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 import Die from './Die'
 import {nanoid} from "nanoid"
+import { faWheatAwnCircleExclamation } from '@fortawesome/free-solid-svg-icons'
 
 export default function App() {
 
   const [dice, setDice] = useState(allNewDice())
-
-
+  
+  function generateNewDie() {
+      return {
+        value: Math.ceil(Math.random() * 6), 
+        isHeld: false,
+        id: nanoid()
+      }
+  }
 
   //Creando 10 numeros random para los dados 
   function allNewDice() {
     const newDice = []
     for(let i = 0; i < 10; i++) {
-      const randomNumber = Math.ceil(Math.random() * 6)
-      newDice.push({
-        value: randomNumber, 
-        isHeld: false,
-        id: nanoid()
-      })
+      newDice.push(generateNewDie())
     }
     return newDice    
   } 
 
-  //Seteando el numero que mostrará cada dado haciendo uso de .map en el array random generado
+    //Funcion para volver a tirar los dados usando el botón 
+    function rollNewDice() {
+      setDice(prevDice => prevDice.map(die => {
+        return die.isHeld ?
+            die: 
+            generateNewDie()             
+      }))
+    
+    }
+  
+    //Funcion para "sostener" los dados al hacer click
+    function holdDice(id) {
+        setDice(prevDice => prevDice.map(die => {
+            return die.id === id ? 
+              {...die, isHeld: !die.isHeld} : 
+              die
+          })
+        )       
+    }
+
+  //Seteando lo que mostrará cada dado haciendo uso de .map en el array que contiene los objetos (dados) generados
   const diceElements = dice.map((die) => {
     return (
       <Die 
         key = {die.id}
         value = {die.value}
+        isHeld = {die.isHeld}
+        holdDice = {() => holdDice(die.id)}
       />
     ) 
   }) 
 
-  //Funcion para volver a tirar los dados usando el botón 
-  function rollNewDice() {
-    setDice(allNewDice())
-  }
+
 
 
   return (
     <main className="main">
       <div className="main-body">
+        <h1 className="title">Tenzies</h1>
+        <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
         <div className="dice--container">
           {diceElements}
         </div>
--- tenziesgame/src/Die.jsx
+++ tenziesgame/src/Die.jsx
@@ -1,8 +1,11 @@
 import React from "react"
 
 export default function Die(props) {
+  const styles = {
+    backgroundColor : props.isHeld ? "#59E391" : "white"
+  }
     return (
-        <div className="die--square">
+        <div className="die--square" style={styles} onClick={props.holdDice}>
             <p className="die--number">{props.value}</p>
         </div>
     )
